6549cb9d9bda63bbf8414048abe42e2c20b2014e
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-08-22 21:51:45 -0300

    refractor: change res.send to res.json

--- src/libs/response.handle.ts
+++ src/libs/response.handle.ts
@@ -10,5 +10,5 @@ import { APIResponse } from "../types/response";
  */
 export function apiResponse<I>(res: Response, apiRes: APIResponse<I>) {
     res.status(apiRes.status);
-    res.send(apiRes);
+    res.json(apiRes);
 }
