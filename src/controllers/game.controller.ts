71ca40458c16f25854f82a1fc2adce96efa1a75d
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-08-22 21:52:19 -0300

    feat: add controller to game

---
+++ src/controllers/game.controller.ts
@@ -0,0 +1,72 @@
+import { Response } from "express";
+import { IQuestion } from "../types/question";
+import { GameParams, TypedRequest } from "../types/request";
+import { isValidId } from "../libs/validObjectId";
+import { apiResponse } from "../libs/response.handle";
+import { StatusCodes } from "http-status-codes";
+import QuestionModel from "../models/question.model";
+import { generateRandomNumbers } from "../libs/random";
+
+export interface GameQuery {
+    category?: string;
+    verified?: boolean;
+}
+
+const playGame = async (
+    req: TypedRequest<IQuestion, GameParams>,
+    res: Response
+) => {
+    // parametro recibido en el path
+    const { category, limit = 1 } = req.params;
+
+    // query para la busqueda de preguntas
+    let query: GameQuery = { verified: true };
+
+    // Arreglo para las preguntas a enviar
+    const questions: Array<IQuestion> = [];
+
+    // Verifico si se envía un id de categoría que este sea válido
+    if (category !== "all" && !isValidId(category))
+        return apiResponse(res, {
+            status: StatusCodes.BAD_REQUEST,
+            message: "Id inválido",
+        });
+
+    // si se envío un ID de categoría, lo asigno a la query
+    if (category && category !== "all") query.category = category;
+
+    try {
+        // Busco todas las preguntas en la BD usando la query
+        const questionsFound = await QuestionModel.find(query).populate(
+            "category"
+        );
+
+        // conjunto de números para preguntas aleatorios
+        const numbersRandom = generateRandomNumbers(
+            questionsFound.length,
+            limit
+        );
+
+        // agrego a "questions" las preguntas encontradas en la BD coincidentes en el index de cada elemento
+        numbersRandom.forEach((e) => questions.push(questionsFound[e]));
+
+        return apiResponse(res, {
+            message: `Total: ${limit} preguntas - Categoría: ${
+                category === "all" ? "Aletario" : questions[0].category.name
+            }`,
+            status: StatusCodes.ACCEPTED,
+            data: questions,
+        });
+    } catch (error) {
+        return apiResponse(res, {
+            status: StatusCodes.INTERNAL_SERVER_ERROR,
+            message: error as string,
+        });
+    }
+};
+
+const gameController = {
+    playGame,
+};
+
+export default gameController;
