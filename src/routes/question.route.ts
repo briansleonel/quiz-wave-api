73c6127cbfa94a43afc53d15038fa6d15d2ff122
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-09-15 15:19:14 -0300

    feat: add user role validation middleware to question route

--- src/routes/question.route.ts
+++ src/routes/question.route.ts
@@ -3,29 +3,39 @@ import questionController from "../controllers/question.controller";
 import { authRequired } from "../middlewares/validateToken.middleware";
 import { validateSchema } from "../middlewares/validateSchema.middleware";
 import { questionSchema } from "../schemas/question.schema";
+import {
+    valiateRoleUserQuestion,
+    validateRoleAdmin,
+} from "../middlewares/validateRole.middleware";
 
 const questionRouter = Router();
 
 questionRouter.get("/question", authRequired, questionController.getAll);
+
 questionRouter.get(
     "/question/:id",
     authRequired,
     questionController.getQuestion
 );
+
 questionRouter.post(
     "/question",
     authRequired,
     validateSchema(questionSchema),
     questionController.addQuestion
 );
+
 questionRouter.put(
     "/question/:id",
     authRequired,
+    valiateRoleUserQuestion,
     validateSchema(questionSchema),
     questionController.updateQuestion
 );
+
 questionRouter.delete(
     "/question/:id",
+    valiateRoleUserQuestion,
     authRequired,
     questionController.deleteQuestion
 );
@@ -33,6 +43,7 @@ questionRouter.delete(
 questionRouter.put(
     "/question/verified/:id",
     authRequired,
+    validateRoleAdmin,
     questionController.changeVerified
 );
 
