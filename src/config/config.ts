5de9b257b0bfe4cb87d3d0a1633f70b9f3cf7687
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-09-15 12:20:07 -0300

    feat: change configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    PORT: Number(process.env.PORT) || 3000,
+    PORT: Number(process.env.PORT) || 3001,
     HOSTNAME: process.env.HOSTNAME || "localhost",
     MONGODB_URI:
         process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/quizz-game",
