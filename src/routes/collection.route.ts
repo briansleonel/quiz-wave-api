2c3209243b8c038fbae85be8c97cab2f5c210bc8
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-17 23:37:35 -0300

    feat: protect collection routes with user authentication

--- src/routes/collection.route.ts
+++ src/routes/collection.route.ts
@@ -1,14 +1,32 @@
 import { Router } from "express";
 import collectionController from "../controllers/collection.controller";
+import { authRequired } from "../middlewares/validateToken.middleware";
 
 const collectionRouter = Router();
 
-collectionRouter.get("/collection", collectionController.getCollectionsQuery);
-collectionRouter.get("/collection/:id", collectionController.getCollection);
-collectionRouter.post("/collection", collectionController.addCollection);
-collectionRouter.put("/collection/:id", collectionController.updateCollection);
+collectionRouter.get(
+    "/collection",
+    authRequired,
+    collectionController.getCollectionsQuery
+);
+collectionRouter.get(
+    "/collection/:id",
+    authRequired,
+    collectionController.getCollection
+);
+collectionRouter.post(
+    "/collection",
+    authRequired,
+    collectionController.addCollection
+);
+collectionRouter.put(
+    "/collection/:id",
+    authRequired,
+    collectionController.updateCollection
+);
 collectionRouter.delete(
     "/collection/:id",
+    authRequired,
     collectionController.deleteCollection
 );
 
