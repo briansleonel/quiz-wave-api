2a79de966662e1b83ff75ad3638b517684320201
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-18 00:43:21 -0300

    feat: implement schema to validate a collection

--- src/routes/collection.route.ts
+++ src/routes/collection.route.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import collectionController from "../controllers/collection.controller";
 import { authRequired } from "../middlewares/validateToken.middleware";
+import { validateSchema } from "../middlewares/validateSchema.middleware";
+import { collectionSchema } from "../schemas/collection.schema";
 
 const collectionRouter = Router();
 
@@ -17,11 +19,13 @@ collectionRouter.get(
 collectionRouter.post(
     "/collection",
     authRequired,
+    validateSchema(collectionSchema),
     collectionController.addCollection
 );
 collectionRouter.put(
     "/collection/:id",
     authRequired,
+    validateSchema(collectionSchema),
     collectionController.updateCollection
 );
 collectionRouter.delete(
