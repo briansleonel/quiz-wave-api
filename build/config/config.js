f5cdf65383676ddff437a5ae02e914e5e41fe0ad
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-04-06 17:28:41 -0300

    clean main branch

--- build/config/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    PORT: Number(process.env.PORT) || 3001,
-    HOSTNAME: process.env.HOSTNAME || "localhost",
-    MONGODB_URI: process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/quizz-game",
-    TOKEN_SECRET: "development",
-};
-exports.default = config;
